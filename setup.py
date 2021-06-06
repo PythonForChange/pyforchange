96ef31d97d92010a0d8bc0692bb6a95604f257f5
Emmanuel Norambuena <eanorambuena@uc.cl>
2021-06-06 17:11:36 +0000

    Setup files have been added.

--- setup.py
+++ setup.py
@@ -5,9 +5,28 @@ setup(
   version = '0.1',
   description = 'my description',
   author = 'Python For Change',
-  author_email = 'eanorambuena@uc.cl',
+  author_email = 'pythonforchange@gmail.com',
   url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
-  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  download_url = 'https://github.com/PythonForChange/pyforchange/tarball/0.1',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
 )
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyforchange',
+  packages = ['pyforchange'], # this must be the same as the name above   
+    include_package_data=True,    # muy importante para que se incluyan archivos sin extension .py
+    version = '0.1',
+    description = ' ',
+    author = 'Python For Change',
+    author_email = 'pythonforchange@gmail.com',
+    license="MIT",
+    url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
+    download_url = 'https://github.com/PythonForChange/pyforchange/tarball/0.1',
+    keywords = ['testing', 'logging', 'example'],
+    classifiers = ["Programming Language :: Python :: 3",\
+        "License :: MIT",\
+        "Development Status :: 1 - Beta", "Intended Audience :: Developers", \
+        "Operating System :: OS Independent"],
+    )
