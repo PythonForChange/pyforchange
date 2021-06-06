e0af0333cddeaf5e229c2a38cd51afa965841268
PythonForChange <>
2021-06-06 16:25:38 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyforchange',
+  packages = ['pyforchange'], # this must be the same as the name above
+  version = '0.1',
+  description = 'my description',
+  author = 'Python For Change',
+  author_email = 'eanorambuena@uc.cl',
+  url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
+  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
