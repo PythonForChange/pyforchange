b2496c4d1cd480fafe90515504e873b49a9fa700
Emmanuel Norambuena <eanorambuena@uc.cl>
2021-06-09 01:49:17 +0000

    Version 0.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name = 'pyforchange',
   packages = ['pyforchange'], # this must be the same as the name above   
     include_package_data=True,    # muy importante para que se incluyan archivos sin extension .py
-    version = '0.1',
+    version = '0.2',
     description = ' ',
     long_description= " :D ",
     author = 'Python For Change',
     author_email = 'pythonforchange@gmail.com',
     license="MIT",
     url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
-    download_url = 'https://github.com/PythonForChange/pyforchange/tarball/0.1',
+    download_url = 'https://github.com/PythonForChange/pyforchange/archive/v2.0.tar.gz',
     keywords = ['testing', 'logging', 'example'],
     classifiers = ['Programming Language :: Python :: 3'],
     )
