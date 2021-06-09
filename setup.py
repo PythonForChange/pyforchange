b385ab1476b054693fbbe84286a02b8b23bf1719
Emmanuel Norambuena <eanorambuena@uc.cl>
2021-06-09 01:39:13 +0000

    Add cell & covidplot

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from distutils.core import setup
-setup(
-  name = 'pyforchange',
-  packages = ['pyforchange'], # this must be the same as the name above
-  version = '0.1',
-  description = 'my description',
-  author = 'Python For Change',
-  author_email = 'pythonforchange@gmail.com',
-  url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
-  download_url = 'https://github.com/PythonForChange/pyforchange/tarball/0.1',
-  keywords = ['testing', 'logging', 'example'],
-  classifiers = [],
-)
 from setuptools import setup, find_packages
 
 setup(
@@ -19,14 +6,12 @@ setup(
     include_package_data=True,    # muy importante para que se incluyan archivos sin extension .py
     version = '0.1',
     description = ' ',
+    long_description= " :D ",
     author = 'Python For Change',
     author_email = 'pythonforchange@gmail.com',
     license="MIT",
     url = 'https://github.com/PythonForChange/pyforchange', # use the URL to the github repo
     download_url = 'https://github.com/PythonForChange/pyforchange/tarball/0.1',
     keywords = ['testing', 'logging', 'example'],
-    classifiers = ["Programming Language :: Python :: 3",\
-        "License :: MIT",\
-        "Development Status :: 1 - Beta", "Intended Audience :: Developers", \
-        "Operating System :: OS Independent"],
+    classifiers = ['Programming Language :: Python :: 3'],
     )
